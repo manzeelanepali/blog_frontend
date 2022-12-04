ca66b8dcbd4d125d928f8830a7e480975cda2fe2
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-04 12:48:21 +0545

    feat : just to check how the toggleable component we added works

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -155,7 +155,7 @@ const App = () => {
 
   return (
     <div>
-      <Togglable buttonLabel="show Me">This is coming from App.js </Togglable>
+      <Togglable buttonLabel="show Me">This is coming from App.js</Togglable>
       <h2>blogs</h2>
       <Notification message={message?.message} type={message?.type} />
       {user === null ? (
