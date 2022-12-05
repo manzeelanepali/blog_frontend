50c9e4e8dbd2f3ac24b5352f49a90b89b353750f
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-05 12:45:37 +0545

    feat : added the css features that makes appear the blog in the descibed css form

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -9,29 +9,11 @@ const Blog = ({ blog }) => {
     borderWidth: 1,
     marginBottom: 5,
   };
-  const showToggle = () => {
-    setDisplay(!display);
-  };
   return (
     <div style={blogStyle}>
-      {!display ? (
-        <div>
-          {blog.title}
-          <button onClick={showToggle}>view</button>
-        </div>
-      ) : (
-        <div>
-          <div>
-            {blog.title}
-            <button onClick={showToggle}>hide</button>
-          </div>
-          <div>{blog.url}</div>
-          <div>
-            likes: {blog.likes} <button>like</button>
-          </div>
-          <div>{blog.author}</div>
-        </div>
-      )}
+      <div>
+        {blog.title} {blog.author}
+      </div>
     </div>
   );
 };
