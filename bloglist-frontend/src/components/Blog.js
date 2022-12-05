2152453b837854877aaab0f4521bb77111122000
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-05 12:40:26 +0545

    feat : Full details of the blog open when the button is clicked. also the inline css has been added

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const [display, setDisplay] = useState(false);
 
-export default Blog
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const showToggle = () => {
+    setDisplay(!display);
+  };
+  return (
+    <div style={blogStyle}>
+      {!display ? (
+        <div>
+          {blog.title}
+          <button onClick={showToggle}>view</button>
+        </div>
+      ) : (
+        <div>
+          <div>
+            {blog.title}
+            <button onClick={showToggle}>hide</button>
+          </div>
+          <div>{blog.url}</div>
+          <div>
+            likes: {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
