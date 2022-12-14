1129d61d5994400c2ea70eec57574fcf3e4c7871
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-14 14:53:43 +0545

    feat : cypress test for checking that the application displays the login form by default.

---
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,11 @@
+describe("Blog app", function () {
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.visit("http://localhost:3000");
+  });
+  it("login form can be opened", function () {
+    cy.contains("login").click();
+    cy.get("input:first").type("anu");
+    cy.get("input:last").type("anu");
+  });
+});
