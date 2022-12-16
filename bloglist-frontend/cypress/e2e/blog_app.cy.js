b486e213f8ed39d68d775871057b91ed0737577c
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-16 15:46:21 +0545

    exercise : 5.18 Make tests for logging in. Test both successful and unsuccessful login attempts

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -3,37 +3,39 @@ describe("Blog app", function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
     cy.visit("http://localhost:3000");
   });
+
   it("login form can be opened", function () {
     cy.contains("login").click();
   });
 
-  it("login fails with wrong password", function () {
+  it.only("login fails with wrong password", function () {
     cy.contains("login").click();
-    cy.get("#username").type("sahil");
-    cy.get("#password").type("wrong");
+    cy.get("#username").type("iran");
+    cy.get("#password").type("kiran");
     cy.get("#login-button").click();
   });
 
   it("user can log in", function () {
     cy.contains("login").click();
-    cy.get("#username").type("sahil");
-    cy.get("#password").type("sahil");
+    cy.get("#username").type("kiran");
+    cy.get("#password").type("kiran");
     cy.get("#login-button").click();
   });
-
-  describe("when logged in", function () {
-    beforeEach(function () {
-      cy.contains("login").click();
-      cy.get("input:first").type("sahil");
-      cy.get("input:last").type("sahil");
-      cy.get("#login-button").click();
-    });
-
-    it("a blog can be created", function () {
-      cy.contains("new blog").click();
-      cy.get("input").type("a note created by cypress");
-      cy.contains("save").click();
-      cy.contains("a note created by cypress");
-    });
-  });
 });
+
+//   describe("when logged in", function () {
+//     beforeEach(function () {
+//       cy.contains("login").click();
+//       cy.get("input:first").type("kiran");
+//       cy.get("input:last").type("kiran");
+//       cy.get("#login-button").click();
+//     });
+
+//     it("a blog can be created", function () {
+//       cy.contains("new blog").click();
+//       cy.get("input").type("a note created by cypress");
+//       cy.contains("save").click();
+//       cy.contains("a note created by cypress");
+//     });
+//   });
+// });
