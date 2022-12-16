156b28a9059e4f25f88e96543590e0558fba530f
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-16 11:17:21 +0545

    feat: feature added . cypress texting 5.18

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -7,17 +7,33 @@ describe("Blog app", function () {
     cy.contains("login").click();
   });
 
-  describe("Login", function () {
-    it("succeeds with correct credentials", function () {
+  it("login fails with wrong password", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("sahil");
+    cy.get("#password").type("wrong");
+    cy.get("#login-button").click();
+  });
+
+  it("user can log in", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("sahil");
+    cy.get("#password").type("sahil");
+    cy.get("#login-button").click();
+  });
+
+  describe("when logged in", function () {
+    beforeEach(function () {
       cy.contains("login").click();
-      cy.get("input:first").type("anu");
-      cy.get("input:last").type("anu");
+      cy.get("input:first").type("sahil");
+      cy.get("input:last").type("sahil");
+      cy.get("#login-button").click();
     });
 
-    it("fails with wrong credentials", function () {
-      cy.contains("login").click();
-      cy.get("input:first").type("anu");
-      cy.get("input:last").type("sanu");
+    it("a blog can be created", function () {
+      cy.contains("new blog").click();
+      cy.get("input").type("a note created by cypress");
+      cy.contains("save").click();
+      cy.contains("a note created by cypress");
     });
   });
 });
