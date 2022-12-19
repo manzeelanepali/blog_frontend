83a27338eddbcc41443cc337932e9ccc44bbb90f
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-19 12:44:26 +0545

    exercise 5.21: Make a test for ensuring that the user who created a blog can delete it.

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -58,5 +58,15 @@ describe("Blog app", function () {
       cy.contains("like").click();
       cy.contains("1");
     });
+    it("user who created the blog can delete it ", function () {
+      cy.contains("new blog").click();
+      cy.get("#title").type("new story created by cypress");
+      cy.get("#author").type("manjila");
+      cy.get("#url").type("hello");
+      cy.get("#button-type").click();
+      cy.contains("view").click();
+      cy.contains("remove").click();
+      cy.contains("a blog created by cypress").should("not.exist");
+    });
   });
 });
